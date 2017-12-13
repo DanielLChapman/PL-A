1e840daa5542d61a3704351d72bfe7389bf36901
RustyRadio <danielc@my.ccsu.edu>
2017-12-13 10:18:02 -0500

    Update helper and readme

--- README.md
+++ README.md
@@ -11,3 +11,10 @@
 - **Database**: mongodb
 - **Authentication**: passport
 - **Deployment**: none
+
+
+Requires a variables.env file with the following: 
+
+NODE_ENV=
+DATABASE=
+PORT=7777
--- helpers.js
+++ helpers.js
@@ -11,12 +11,5 @@ exports.moment = require('moment');
 // Dump is a handy debugging function we can use to sort of "console.log" our data
 exports.dump = (obj) => JSON.stringify(obj, null, 2);
 
-// inserting an SVG
-exports.icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
-
 // Some details about the site
 exports.siteName = `PL-A`;
-
-exports.menu = [
-  //{ slug: '/stores', title: 'Stores', icon: 'store', }
-];
